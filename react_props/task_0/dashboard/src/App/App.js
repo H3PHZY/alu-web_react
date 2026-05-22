e6cf1bbaf971e0c7cce0c89acd7cd6a220823d99
Hephzibah Ofomi <h.ofomi@alustudent.com>
2026-05-22 01:47:17 +0100

    feat: convert App function to React class

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 
-function App() {
-  return (
-    <>
-      <Notifications />
-      <div className="App">
-        <Header />
-        <body className="App-body">
-          <Login />
-        </body>
-        <Footer />
-      </div>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <body className="App-body">
+            <Login />
+          </body>
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
