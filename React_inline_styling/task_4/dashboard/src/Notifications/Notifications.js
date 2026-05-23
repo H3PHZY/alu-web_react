45b8c4dfd4b7a16ebe113f82727b82582cfcc4b5
Hephzibah Ofomi <h.ofomi@alustudent.com>
2026-05-23 19:04:26 +0100

    feat: add keyframe animations and visibility toggles to Notifications in task_4

---
+++ React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,138 @@
+import React from "react";
+import close from "../assets/close-icon.png";
+import NotificationItem from "./NotificationItem";
+import PropTypes from "prop-types";
+import NotificationItemShape from "./NotificationItemShape";
+import { StyleSheet, css } from "aphrodite";
+
+class Notifications extends React.Component {
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+  shouldComponentUpdate(nextProps) {
+    return (
+      nextProps.listNotifications.length > this.props.listNotifications.length
+    );
+  }
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+    return (
+      <>
+        {!displayDrawer && (
+          <div className={css(styles.menuItem)}>
+            Your notifications
+          </div>
+        )}
+        {displayDrawer && (
+          <div className={css(styles.Notifications)}>
+            <p className={css(styles.text)}>
+              Here is the list of notifications{" "}
+              <button
+                aria-label="close"
+                style={{ textAlign: "right", display: "inline" }}
+                onClick={() => console.log("Close button has been clicked")}
+              >
+                <img src={close} alt="close" height={20} width={20} />
+              </button>
+            </p>
+            <ul className={css(styles.list)}>
+              {listNotifications.length > 0 ? (
+                listNotifications.map(({ id, type, value, html }) => (
+                  <NotificationItem
+                    id={id}
+                    markAsRead={this.markAsRead}
+                    key={id}
+                    type={type}
+                    value={value}
+                    html={html}
+                  />
+                ))
+              ) : (
+                <NotificationItem value="No new notification for now" />
+              )}
+            </ul>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+const opacityKeyframes = {
+  'from': {
+    opacity: 0.5,
+  },
+  'to': {
+    opacity: 1,
+  }
+};
+
+const bounceKeyframes = {
+  '0%': {
+    transform: 'translateY(0px)',
+  },
+  '50%': {
+    transform: 'translateY(-5px)',
+  },
+  '100%': {
+    transform: 'translateY(5px)',
+  }
+};
+
+const styles = StyleSheet.create({
+  Notifications: {
+    border: "2px solid pink",
+    padding: "20px",
+    '@media (max-width: 900px)': {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      zIndex: 9999,
+      backgroundColor: "white",
+      border: "none",
+      padding: 0,
+    }
+  },
+
+  text: { 
+    display: "flex", 
+    justifyContent: "space-between",
+    '@media (max-width: 900px)': {
+      fontSize: "20px",
+    }
+  },
+
+  list: {
+    '@media (max-width: 900px)': {
+      padding: 0,
+      listStyle: "none",
+      margin: 0,
+      fontSize: "20px",
+    }
+  },
+
+  menuItem: {
+    float: "right",
+    backgroundColor: "#fff8f8",
+    cursor: "pointer",
+    ':hover': {
+      animationName: [opacityKeyframes, bounceKeyframes],
+      animationDuration: '1s, 0.5s',
+      animationIterationCount: 3,
+    }
+  }
+});
+
+export default Notifications;
---
+++ React_inline_styling/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,70 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { getLatestNotification } from "../utils/utils";
+import Notifications from "./Notifications";
+import { render, screen } from "@testing-library/react";
+import { StyleSheetTestUtils } from "aphrodite";
+
+StyleSheetTestUtils.suppressStyleInjection()
+
+describe("Notifications tests", () => {
+  it("should render the menuItem when displayDrawer is false", () => {
+    render(<Notifications displayDrawer={false} />);
+    screen.getByText("Your notifications");
+  });
+
+  it("should not render div.Notifications when displayDrawer is false", () => {
+    render(<Notifications displayDrawer={false} />);
+    expect(screen.queryByText("Here is the list of notifications")).toBeNull();
+  });
+
+  it("should not render menuItem when displayDrawer is true", () => {
+    render(<Notifications displayDrawer={true} />);
+    expect(screen.queryByText("Your notifications")).toBeNull();
+  });
+
+  it("should render div.Notifications when displayDrawer is true", () => {
+    render(<Notifications displayDrawer={true} />);
+    screen.getByText("Here is the list of notifications");
+  });
+
+  it("should render the correct message when there is no notification", () => {
+    render(<Notifications displayDrawer={true} />);
+    expect(screen.getAllByRole("listitem")).toHaveLength(1);
+  });
+
+  it("should render all notifications", () => {
+    const listNotifications = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "urgent", html: getLatestNotification() },
+    ];
+    render(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+    expect(screen.getAllByRole("listitem")).toHaveLength(3);
+  });
+
+  it("should render the correct text", () => {
+    render(<Notifications displayDrawer={true} />);
+    screen.getByText("Here is the list of notifications");
+  });
+
+  it("should update component when listNotifications prop changes", () => {
+    const listNotifications = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+    ];
+    const { rerender } = render(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+    expect(screen.getAllByRole("listitem")).toHaveLength(2);
+
+    const updatedListNotifications = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "urgent", value: "New notification" },
+    ];
+    rerender(<Notifications displayDrawer={true} listNotifications={updatedListNotifications} />);
+    expect(screen.getAllByRole("listitem")).toHaveLength(3);
+  });
+});
