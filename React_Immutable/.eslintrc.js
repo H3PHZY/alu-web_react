e0e44ca3f1cbe2d4f83e6d517698b60db07987a4
Hephzibah Ofomi <h.ofomi@alustudent.com>
2026-05-30 14:15:09 +0100

    Initialize React_Immutable project with config files and implement 0-fromjs.js

---
+++ React_Immutable/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
