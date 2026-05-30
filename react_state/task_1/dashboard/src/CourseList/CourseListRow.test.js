bf6e5e692d3b50b0707cd2b6c9b365fcf30b3f14
Hephzibah Ofomi <h.ofomi@alustudent.com>
2026-05-30 13:24:36 +0100

    Implement login form, controlled inputs and tests for task_1

---
+++ react_state/task_1/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,56 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { StyleSheetTestUtils } from 'aphrodite';
+StyleSheetTestUtils.suppressStyleInjection();
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseListRow from './CourseListRow';
+
+describe('<CourseListRow />', () => {
+    it('renders an <CourseListRow /> component', () => {
+        const wrapper = shallow(<CourseListRow textFirstCell="test" />);
+        expect(wrapper).toHaveLength(1);
+    });
+
+    it('renders a <CourseListRow /> component with isHeader set to true and textSecondCell === null', () => {
+        const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" />);
+        expect(wrapper.find('th')).toHaveLength(1);
+        expect(wrapper.find('th').prop('colSpan')).toEqual('2');
+    });
+
+    it('tests the component renders two cells when textSecondCell is present', () => {
+        const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test2" />);
+        expect(wrapper.find('th')).toHaveLength(2);
+    });
+
+    it('tests the component renders correctly two td elements within a tr element when isHeader is false', () => {
+        const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test2" />);
+        expect(wrapper.find('tr td')).toHaveLength(2);
+    });
+
+    it('applies style for headerRow when isHeader is true', () => {
+        const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" />);
+        expect(wrapper.find('tr').prop('className')).toContain('headerRow');
+    });
+
+    it('applies style for defaultRow when isHeader is false', () => {
+        const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" />);
+        expect(wrapper.find('tr').prop('className')).toContain('defaultRow');
+    });
+
+    it('applies the correct styling for th elements when textSecondCell is null', () => {
+        const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell={null} />);
+        expect(wrapper.find('th').prop('className')).toContain('thStyle');
+    });
+
+    it('applies the correct styling for th elements when textSecondCell is not null', () => {
+        const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test2" />);
+        wrapper.find('th').forEach((node) => {
+            expect(node.prop('className')).toContain('thStyle');
+            expect(node.prop('className')).toContain('thSubHeaderStyle');
+        });
+    });
+});
