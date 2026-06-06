fe095c3c58dfd9d83f0c1854838431b4372c7035
Hephzibah Ofomi <h.ofomi@alustudent.com>
2026-06-06 19:41:21 +0100

    task_0: Create basic state uiReducer

---
+++ react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,23 @@
+import { uiReducer, initialState } from "./uiReducer";
+import { DISPLAY_NOTIFICATION_DRAWER } from "../actions/uiActionTypes";
+import { SELECT_COURSE } from "../actions/courseActionTypes";
+
+describe("uiReducer", () => {
+  it("returns the initial state when no action is passed", () => {
+    const state = uiReducer(undefined, {});
+    expect(state).toEqual(initialState);
+  });
+
+  it("returns the initial state when the action SELECT_COURSE is passed", () => {
+    const state = uiReducer(undefined, { type: SELECT_COURSE });
+    expect(state).toEqual(initialState);
+  });
+
+  it("changes correctly the isNotificationDrawerVisible property when the action DISPLAY_NOTIFICATION_DRAWER is passed", () => {
+    const state = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
+    expect(state).toEqual({
+      ...initialState,
+      isNotificationDrawerVisible: true,
+    });
+  });
+});
